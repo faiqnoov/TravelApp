d50386a4119d05e86d3a107dc9cb19c7577ac1e3
faiqnoov <mfaiqn@gmail.com>
2022-04-05 12:09:28 +0700

    prak04 - tugas

--- lib/done_tourism_list.dart
+++ lib/done_tourism_list.dart
@@ -23,9 +23,29 @@ class DoneTourismList extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget> [
-                Text(
-                  place.name,
-                  style: const TextStyle(fontSize: 16.0),
+                Expanded(
+                  flex: 1,
+                  child: Image.asset(place.imageAsset[0]),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget> [
+                        Text(
+                          place.name,
+                          style: const TextStyle(fontSize: 16.0),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Text(place.location),
+                      ],
+                    ),
+                  ),
                 ),
                 const Icon(Icons.done_outline),
               ],
