64c29ab2daceac16657c09002b4f359cad9bb8c5
faiqnoov <mfaiqn@gmail.com>
2022-04-05 10:31:03 +0700

    prak04 - state - setState()

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'detail_screen.dart';
 import 'main_screen.dart';
 
 void main() {
@@ -15,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Traveloke',
       theme: ThemeData(),
-      home: const MainScreen(),
+      home: MainScreen(),
     );
   }
 }
--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,64 +1,40 @@
 import 'package:flutter/material.dart';
-import 'package:travelapp/detail_screen.dart';
+import 'package:travelapp/done_tourism_list.dart';
 import 'package:travelapp/model/tourism_place.dart';
+import 'package:travelapp/tourism_list.dart';
 
-class MainScreen extends StatelessWidget {
-  const MainScreen({Key? key}) : super(key: key);
+class MainScreen extends StatefulWidget {
+  MainScreen({Key? key}) : super(key: key);
+
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  final List<TourismPlace> doneTourismPlaceList = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Wisata Surabaya'),
-      ),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          final TourismPlace place = tourismPlaceList[index];
-          return InkWell(
-            onTap: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) {
-                return DetailScreen(place: place);
-              }));
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.done_outline),
+            onPressed: (){
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) {
+                  return DoneTourismList(
+                      doneTourismPlaceList: doneTourismPlaceList
+                  );
+                }),
+              );
             },
-            child: ListItem(place),
-          );
-        },
-        itemCount: tourismPlaceList.length,
-      ),
-    );
-  }
-
-  Widget ListItem(TourismPlace place) {
-    return Card(
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget> [
-          Expanded(
-              flex: 1,
-              child: Image.asset(place.imageAsset[0]),
-          ),
-          Expanded(
-            flex: 2,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget> [
-                  Text(
-                    place.name,
-                    style: const TextStyle(fontSize: 16.0),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Text(place.location),
-                ],
-              ),
-            ),
-          ),
+          )
         ],
       ),
+      body: TourismList(doneTourismPlaceList: doneTourismPlaceList),
     );
   }
 }
